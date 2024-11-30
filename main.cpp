8399ddeffa1139864010d04c665ebae61606adad
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-11-30 21:58:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,26 +10,43 @@ void kolor(int k)
 {
     SetConsoleTextAttribute(color,k);
 }
-int dodawanie(int a, int b){
+void logo()
+{
+    kolor(13);cout<<R"(
+ ___  __    ________  ___       ___  __    ___  ___  ___       ________  _________  ________  ________  ________
+|\  \|\  \ |\   __  \|\  \     |\  \|\  \ |\  \|\  \|\  \     |\   __  \|\___   ___\\   __  \|\   __  \|\   __  \
+\ \  \/  /|\ \  \|\  \ \  \    \ \  \/  /|\ \  \\\  \ \  \    \ \  \|\  \|___ \  \_\ \  \|\  \ \  \|\  \ \  \|\  \
+ \ \   ___  \ \   __  \ \  \    \ \   ___  \ \  \\\  \ \  \    \ \   __  \   \ \  \ \ \  \\\  \ \   _  _\ \   _  _\
+  \ \  \\ \  \ \  \ \  \ \  \____\ \  \\ \  \ \  \\\  \ \  \____\ \  \ \  \   \ \  \ \ \  \\\  \ \  \\  \\ \  \\  \|
+   \ \__\\ \__\ \__\ \__\ \_______\ \__\\ \__\ \_______\ \_______\ \__\ \__\   \ \__\ \ \_______\ \__\\ _\\ \__\\ _\
+    \|__| \|__|\|__|\|__|\|_______|\|__| \|__|\|_______|\|_______|\|__|\|__|    \|__|  \|_______|\|__|\|__|\|__|\|__|)";
+cout<<endl;
+}
+int dodawanie(int a, int b)
+{
     return a+b;
 }
-
-int dzieleniecalk(int a, int b){
+int dzieleniecalk(int a, int b)
+{
     return a/b;
 }
-
 string dziesietnyNaSystem(int liczba, int system){
+    if (system < 2 || system > 10) {
+        return "Nieprawidlowy system (2-10)";
+    }
     string wynik;
+    if (liczba == 0) return "0";
     while(liczba>0){
         wynik+=to_string(liczba%system);
         liczba/=system;
     }
     reverse(wynik.begin(), wynik.end());
-    return wynik.empty()?"0":wynik;
+    return wynik;
 }
 
 int systemNaDziesiet(const string& liczba, int system)
 {
+    int wynik = 0;
     for(char cyfra:liczba){
         wynik=wynik*system+(cyfra-'0');
     }
@@ -40,7 +57,6 @@ string systemNaSystem(const string& liczba, int systemWej, int systemWyj){
     int dziesietny=systemNaDziesiet(liczba, systemWej);
     return dziesietnyNaSystem(dziesietny, systemWyj);
 }
-
 void menu()
 {
     kolor(11);cout<<"Wybierz dzialanie:\n";kolor(9);
@@ -49,9 +65,9 @@ void menu()
     cout<<" 5. Przeliczanie liczby z dowolnego systemu liczbowego na dowolny\n 0. Wyjscie\n";
     cout<<"Twoj wybor: ";kolor(89);cout<<"[   ]\b\b\b";cin>>wybor;kolor(7);
 }
-
-int main(){
-
+int main()
+{
+    logo();
     do{
         menu();
         switch(wybor){
@@ -60,7 +76,7 @@ int main(){
                 int a,b;
                 cout<<"Podaj dwie liczby do dodania (oddzielone spacja): ";
                 cin>>a>>b;
-                cout<<"Wynik: "<<dodawanie(a,b)<<endl;break;
+                kolor(14);cout<<"Wynik: "<<dodawanie(a,b)<<endl;break;
                 }
 
             case 2:
@@ -68,14 +84,14 @@ int main(){
                 int a,b;
                 cout<<"Podaj dwie liczby do dzielenia calkowitego (oddzielone spacja): ";
                 cin>>a>>b;
-                cout<<"Wynik: "<<dzieleniecalk(a,b)<<endl;break;
+                kolor(14);cout<<"Wynik: "<<dzieleniecalk(a,b)<<endl;break;
                 }
             case 3:
                 {
                 int liczba,system;
                 cout<<"Podaj liczbe dziesietna: ";cin>>liczba;
                 cout<<"Podaj system docelowy: ";cin>>system;
-                cout<<"Wynik: "<<dziesietnyNaSystem(liczba,system)<<endl;
+                kolor(14);cout<<"Wynik: "<<dziesietnyNaSystem(liczba,system)<<endl;
                 break;
                 }
             case 4:
@@ -83,15 +99,16 @@ int main(){
                 string liczba;int system;
                 cout<<"Podaj liczbe w systemie zrodlowym: ";cin>>liczba;
                 cout<<"Podaj system zrodlowy: ";cin>>system;
-                cout<<"Wynik: "<<systemNaDziesiet(liczba,system)<<endl;
+                kolor(14);cout<<"Wynik: "<<systemNaDziesiet(liczba,system)<<endl;
                 break;
                 }
             case 5:
                 {
                 string liczba;
                 int systemwej,systemwyj;
-                cout<<"Podaj liczbe w systemie zrodlowym, system zrodlowy (od 2 do 9) i system docelowy (od 2 do 9): ";
-                cin>>liczba>>systemwej>>systemwyj;
+                cout<<"Podaj liczbe w systemie zrodlowym:";cin>>liczba;
+                cout<<"Podaj system zrodlowy:";cin>>systemwej;
+                cout<<"Podaj system docelowy:";cin>>systemwyj;
                 kolor(14);cout<<"Wynik: "<<systemNaSystem(liczba,systemwej,systemwyj)<<endl;
                 break;
                 }
